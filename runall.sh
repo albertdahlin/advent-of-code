4ceda13ddcefeaf5ac83a403e3e58cacaa89ee63
Albert Dahlin <info@albertdahlin.com>
2024-12-10 12:43:54 +0100

    Print as table

--- runall.sh
+++ runall.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
+case $1 in
+    "c")
+        RUNTIME=""
+        FILE=bin/2024/day
+        EXT=c
+        ;;
+
+    "js")
+        RUNTIME=node
+        FILE=javascript/2024/day
+        EXT=js
+        ;;
+    *)
+        echo "Usage: $0 <lang>"
+        exit 1
+        ;;
+esac
+
 for i in {1..25}; do
     N=$(printf "%02d" $i)
 
-    if [ -f bin/2024/day$N.c ]; then
-        printf "\n# Day $N\n"
-        time ./bin/2024/day$N.c < input/2024/day$N.txt
+    if [ -f $FILE$N.$EXT ]; then
+        printf "% 2s " $i
+        if [ $RUNTIME ]; then
+            IFS=$'\n' read -r -d '' -a OUT <<< $( { time $RUNTIME $FILE$N.$EXT < input/2024/day$N.txt; } 2>&1 )
+        else
+            IFS=$'\n' read -r -d '' -a OUT <<< $( { time $FILE$N.$EXT < input/2024/day$N.txt; } 2>&1 )
+        fi
+        printf "% 7s" ${OUT[3]#user	0m}
+        printf "% 15s" ${OUT[0]}
+        printf "% 15s" ${OUT[1]}
+        echo ""
     fi
 done
 
